3c7129d327c9928082b47f9793ce4c145d213964
Ryan <ryan.leonard71@gmail.com>
2016-04-25 10:56:52 -0700

    Finished refactoring Register File and its test bed

--- Rf.v
+++ Rf.v
@@ -1,29 +1,30 @@
-// 2016 Ryan Leonard
+// 2016 Ryan Leonawrite_addr
 // RegisterFile (RF) Module
 //
 //  The internal data structure is a collection of 
-//  32 registers each 32 bits in size. This is represented
+//  32 registeread_addr_s each 32 bits in size. This is represented
 //  using the 'memories' data structure as discussed in:
 //    http://www.verilogtutorial.info/chapter_3.htm
 
 `timescale 1ns / 1ns;
 module rf_32(
-  input wire        clk,
-  input wire [4:0]  rs, rt, rd,
+  input wire        clock,
+  input wire        reset,
+  input wire [4:0]  read_addr_s, read_addr_t, write_addr,
   input wire        write_enabled,
   input wire [31:0] write_data,
   output reg [31:0] outA, outB
 );
 
-// Set all of our registers to logical default values
+// Set all of our registeread_addr_s to logical default values
 initial 
-  begin
-    outA <= 0;
-    outB <= 0;
-  end
+begin
+  outA <= 0;
+  outB <= 0;
+end
 
 // A 'memories' data structure representing:
-//    32 registers each 32 bits
+//    32 registeread_addr_s each 32 bits
 reg [31:0] register_file[31:0];
 //    |                   |
 //    v                   v
@@ -31,34 +32,28 @@ reg [31:0] register_file[31:0];
 //  (register size)   (register file size)
 
 
-/* 
-//Initialize all registers to zero
-integer i;
-initial 
-  for (i=0; i<=31; i=i+1) begin
-    register_file[i] = 32'b0;
-  end
-*/
-
-//Initialize zero register to zero.
-initial
-  register_file[0] <= 32'b0;
-
-// Using continuous assignment for our outputing
-//assign outA = register_file[rs];
-//assign outB = register_file[rt];
-
 // Using an always block for inputs into our memory array
-always @ (posedge clk)
-    if (write_enabled && rd != 5'd0)
-      register_file[rd] <= write_data;
+integer i;
+always @ (posedge clock)
+begin 
+  if (reset) 
+  begin // BEG reset
+  for (i=0; i<=31; i=i+1)
+    register_file[i] = 32'b0;
+  end // END reset
+  else
+  begin // BEG logic
+    if (write_enabled && write_addr != 5'd0)
+      register_file[write_addr] <= write_data;
+  end // END logic
+end
 
 // Read at the negative edge of the clock to ensure 'read-after-write' doesn't
 // occur
-always @ (negedge clk)
+always @ (negedge clock)
   begin
-    outA <= register_file[rs];
-    outB <= register_file[rt];
+    outA <= register_file[read_addr_s];
+    outB <= register_file[read_addr_t];
   end
 
 
--- Rf_tb.v
+++ Rf_tb.v
@@ -1,183 +1,197 @@
-// 2016 Ryan Leonard
+// 2016 Ryan Leonawrite_addr
 // RegisterFile (RF) Module Testbench
 
 `timescale 1ns / 1ns;
 module test_rf_32;
 
-// The reg/nets we will maniupulate/monitor for testing
-reg         clock;    //clock
-reg [4:0]   rs;             //input
-reg [4:0]   rt;             //input
-reg [4:0]   rd;             //input
-reg [31:0]  write_data;     //input
-reg         we;             //input
-wire [31:0] out_rs;         //output
-wire [31:0] out_rt;         //output
+// The reg/nets write_enabled will maniupulate/monitor for testing
+reg         clock;            //clock
+reg         reset;            //reset
+reg [4:0]   read_addr_s;      //input
+reg [4:0]   read_addr_t;      //input
+reg [4:0]   write_addr;       //input
+reg [31:0]  write_data;       //input
+reg         write_enabled;    //input
+wire [31:0] read_data_s;  //output
+wire [31:0] read_data_t;  //output
 
-// build a version of the Design Under Test (dut)
+// build a veread_addr_sion of the Design Under Test (dut)
 rf_32 dut (
-  .clk            (clock),
-  .rs             (rs), 
-  .rt             (rt), 
-  .rd             (rd),
+  .clock          (clock),
+  .reset          (reset),
+  .read_addr_s    (read_addr_s), 
+  .read_addr_t    (read_addr_t), 
+  .write_addr     (write_addr),
   .write_data     (write_data),
-  .write_enabled  (we),
-  .outA           (out_rs), 
-  .outB           (out_rt)
+  .write_enabled  (write_enabled),
+  .outA           (read_data_s), 
+  .outB           (read_data_t)
 );
 
 
 // Clock Generator
 initial 
+begin
+  clock = 1; 
+  #5;
+  forever
   begin
-    #5 clock = 0;
-    forever
-      #5 clock = ~clock;
+    clock = ~clock; 
+    #5;
   end
+end
 
 // Test stimulus
 integer i;
 initial
-  begin
-    rs=5'd0; rt=5'd0; rd=5'd0; 
-    we=1'b0; write_data=32'h0;
+begin // BEG test
+  reset = 1; 
+  read_addr_s=0; 
+  read_addr_t=0;
+  write_addr=0; 
+  write_enabled=0;
+  write_data=0;
+  #20;
+  reset = 1'b0; // turn off reset for future testing
 
-    //////////////////////////////////////////////////////////// 
-    /// Testing For initialization Correctness
-    //////////////////////////////////////////////////////////// 
-    #100
-    $display("==========\nCheck Initialized to 'dont care'\n");
-    for (i=0; i<=30; i=i+2) begin
-      #10 rs=i; rt=i+1;
-    end
-    rs=0; rt=0;
-
-    //////////////////////////////////////////////////////////// 
-    /// Testing Write
-    //////////////////////////////////////////////////////////// 
-    #100
-    $display("==========\nWrite Some Data to Register File\n");
-    #10 rd=5'd0; we=1'b1; write_data=32'hDEADBEEF;
-    #10 rd=5'd1; we=1'b1; write_data=32'h00000000;
-    #10 rd=5'd2; we=1'b1; write_data=32'h11111111;
-    #10 rd=5'd3; we=1'b1; write_data=32'h22222222;
-    #10 rd=5'd4; we=3'b1; write_data=32'h33333333;
-    #10 rd=5'd5; we=1'b1; write_data=32'h44444444;
-    #10 rd=5'd6; we=1'b1; write_data=32'h55555555;
-    #10 rd=5'd7; we=1'b1; write_data=32'h66666666;
-    #10 rd=5'd8; we=1'b1; write_data=32'h77777777;
-    #10 rd=5'd9; we=1'b1; write_data=32'h88888888;
-    #10 rd=5'd10; we=1'b1; write_data=32'h99999999;
-    #10 rd=5'd11; we=1'b1; write_data=32'hAAAAAAAA;
-    #10 rd=5'd12; we=1'b1; write_data=32'hBBBBBBBB;
-    #10 rd=5'd13; we=1'b1; write_data=32'hCCCCCCCC;
-    #10 rd=5'd14; we=1'b1; write_data=32'hDDDDDDDD;
-    #10 rd=5'd15; we=1'b1; write_data=32'hEEEEEEEE;
-    #10 rd=5'd16; we=1'b1; write_data=32'hFFFFFFFF;
-    #10 rd=5'd17; we=1'b1; write_data=32'h00000001;
-    #10 rd=5'd18; we=1'b1; write_data=32'h00000002;
-    #10 rd=5'd19; we=1'b1; write_data=32'h00000003;
-    #10 rd=5'd20; we=1'b1; write_data=32'h00000004;
-    #10 rd=5'd21; we=1'b1; write_data=32'h00000005;
-    #10 rd=5'd22; we=1'b1; write_data=32'h00000006;
-    #10 rd=5'd23; we=1'b1; write_data=32'h00000007;
-    #10 rd=5'd24; we=1'b1; write_data=32'h00000008;
-    #10 rd=5'd25; we=1'b1; write_data=32'h00000009;
-    #10 rd=5'd26; we=1'b1; write_data=32'h0000000A;
-    #10 rd=5'd27; we=1'b1; write_data=32'h0000000B;
-    #10 rd=5'd28; we=1'b1; write_data=32'h0000000C;
-    #10 rd=5'd29; we=1'b1; write_data=32'h0000000D;
-    #10 rd=5'd30; we=1'b1; write_data=32'h0000000E;
-    #10 rd=5'd31; we=1'b1; write_data=32'hDEADBEEF;
-    #10 we=0;
-
-    //////////////////////////////////////////////////////////// 
-    /// Generally checking the register values real quick
-    //////////////////////////////////////////////////////////// 
-    #100
-    $display("==========\nCheck the written values\n");
-    for (i=0; i<=30; i=i+2) begin
-      #10 rs=i; rt=i+1;
-    end
-
-    //////////////////////////////////////////////////////////// 
-    /// Testing RS and RT indapendently
-    //////////////////////////////////////////////////////////// 
-    #100
-    $display("==========\nRead (RT) Some From Register File\n");
-    #10 rt=5'd0; 
-    #10 rt=5'd1;
-    #10 rt=5'd2; 
-    #10 rt=5'd3; 
-    #10 rt=5'd4; 
-    #10 rt=5'd5; 
-    #10 rt=5'd6; 
-    #10 rt=5'd7; 
-    #10 rt=5'd8; 
-    #10 rt=5'd9; 
-    #10 rt=5'd10;
-    #10 rt=5'd11;
-    #10 rt=5'd12;
-    #10 rt=5'd13;
-    #10 rt=5'd14;
-    #10 rt=5'd15;
-    #10 rt=5'd16;
-    #10 rt=5'd17;
-    #10 rt=5'd18;
-    #10 rt=5'd19;
-    #10 rt=5'd20;
-    #10 rt=5'd21;
-    #10 rt=5'd22;
-    #10 rt=5'd23;
-    #10 rt=5'd24;
-    #10 rt=5'd25;
-    #10 rt=5'd26;
-    #10 rt=5'd27;
-    #10 rt=5'd28;
-    #10 rt=5'd29;
-    #10 rt=5'd30;
-    #10 rt=5'd31;
-    #100
-    $display("==========\nRead (RS) Some From Register File\n");
-    #10 rs=5'd0; 
-    #10 rs=5'd1;
-    #10 rs=5'd2; 
-    #10 rs=5'd3; 
-    #10 rs=5'd4; 
-    #10 rs=5'd5; 
-    #10 rs=5'd6; 
-    #10 rs=5'd7; 
-    #10 rs=5'd8; 
-    #10 rs=5'd9; 
-    #10 rs=5'd10;
-    #10 rs=5'd11;
-    #10 rs=5'd12;
-    #10 rs=5'd13;
-    #10 rs=5'd14;
-    #10 rs=5'd15;
-    #10 rs=5'd16;
-    #10 rs=5'd17;
-    #10 rs=5'd18;
-    #10 rs=5'd19;
-    #10 rs=5'd20;
-    #10 rs=5'd21;
-    #10 rs=5'd22;
-    #10 rs=5'd23;
-    #10 rs=5'd24;
-    #10 rs=5'd25;
-    #10 rs=5'd26;
-    #10 rs=5'd27;
-    #10 rs=5'd28;
-    #10 rs=5'd29;
-    #10 rs=5'd30;
-    #10 rs=5'd31;
+  //////////////////////////////////////////////////////////// 
+  /// Testing For initialization Correctness
+  //////////////////////////////////////////////////////////// 
+  $display("==========\nCheck Reset all to Zero (using s & t)\n");
+  for (i=0; i<=30; i=i+2) begin
+    read_addr_s=i; 
+    read_addr_t=i+1;
+    #10;
   end
+
+  //////////////////////////////////////////////////////////// 
+  /// Testing Write
+  //////////////////////////////////////////////////////////// 
+  $display("==========\nWrite Some Data to Register File\n");
+  read_addr_s=0;  
+  read_addr_t=0;
+  write_enabled=1'b1; 
+  write_addr=5'd0; 
+  write_data=32'hDEADBEEF;
+  #10;
+  write_addr=5'd1; 
+  write_data=32'h00000000;
+  #10 
+  write_addr=5'd2; 
+  write_data=32'h11111111;
+  #10;
+  write_addr=5'd3; 
+  write_data=32'h22222222;
+  #10;
+  write_addr=5'd4; 
+  write_data=32'h33333333;
+  #10;
+  write_addr=5'd5; 
+  write_data=32'h44444444;
+  #10;
+  write_addr=5'd6; 
+  write_data=32'h55555555;
+  #10;
+  write_addr=5'd7; 
+  write_data=32'h66666666;
+  #10;
+  write_addr=5'd8; 
+  write_data=32'h77777777;
+  #10;
+  write_addr=5'd9; 
+  write_data=32'h88888888;
+  #10;
+  write_addr=5'd10; 
+  write_data=32'h99999999;
+  #10;
+  write_addr=5'd11; 
+  write_data=32'hAAAAAAAA;
+  #10;
+  write_addr=5'd12; 
+  write_data=32'hBBBBBBBB;
+  #10;
+  write_addr=5'd13; 
+  write_data=32'hCCCCCCCC;
+  #10;
+  write_addr=5'd14; 
+  write_data=32'hDDDDDDDD;
+  #10;
+  write_addr=5'd15; 
+  write_data=32'hEEEEEEEE;
+  #10;
+  write_addr=5'd16; 
+  write_data=32'hFFFFFFFF;
+  #10;
+  write_addr=5'd17; 
+  write_data=32'h00000001;
+  #10;
+  write_addr=5'd18; 
+  write_data=32'h00000002;
+  #10;
+  write_addr=5'd19; 
+  write_data=32'h00000003;
+  #10;
+  write_addr=5'd20; 
+  write_data=32'h00000004;
+  #10;
+  write_addr=5'd21; 
+  write_data=32'h00000005;
+  #10;
+  write_addr=5'd22; 
+  write_data=32'h00000006;
+  #10;
+  write_addr=5'd23; 
+  write_data=32'h00000007;
+  #10;
+  write_addr=5'd24; 
+  write_data=32'h00000008;
+  #10;
+  write_addr=5'd25; 
+  write_data=32'h00000009;
+  #10;
+  write_addr=5'd26; 
+  write_data=32'h0000000A;
+  #10;
+  write_addr=5'd27; 
+  write_data=32'h0000000B;
+  #10;
+  write_addr=5'd28; 
+  write_data=32'h0000000C;
+  #10;
+  write_addr=5'd29; 
+  write_data=32'h0000000D;
+  #10;
+  write_addr=5'd30; 
+  write_data=32'h0000000E;
+  #10;
+  write_addr=5'd31; 
+  write_data=32'hDEADBEEF;
+  #10;
+  write_enabled=0;
+  write_data=32'h0;
+
+  //////////////////////////////////////////////////////////// 
+  /// Testing RS and RT indapendently
+  //////////////////////////////////////////////////////////// 
+  $display("==========\nRead (RS) Some From Register File\n");
+  for (i=0; i<32; i=i+1) 
+  begin
+    read_addr_s=i; 
+    #10;
+  end
+  $display("==========\nRead (RT) Some From Register File\n");
+  for (i=0; i<32; i=i+1) 
+  begin
+    read_addr_t=i; 
+    #10;
+  end
+end // END testing
 
 // Basic console output
 initial 
   begin
-    $monitor("%d, %d(rs), %d(rt), %d(rd), %b(we), %d(wd), %d(rs_d), %d(rt_d)", 
-      $time, rs, rt, rd, we, write_data, out_rs, out_rt); 
+    $monitor("%d, %d(read_addr_s), %d(read_addr_t), %d(write_addr), %b(write_enabled), %d(write_data), %d(read_data_s), %d(read_data_t)", 
+      $time, read_addr_s, read_addr_t, write_addr, write_enabled, write_data, read_data_s, read_data_t); 
     //$monitor("RF: %p", dut.register_file);
   end
 
