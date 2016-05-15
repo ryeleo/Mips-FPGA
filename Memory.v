7b3ae0b1e3cb094668ae24a12aff7cf7a08ee500
Ryan <ryan.leonard71@gmail.com>
2016-05-15 15:44:49 -0700

    Finished basic memory module and some testing.
    A little more testing to do.
    
    Takes two parameters when creating a module, the memory_size and the
    word_size. Haven't tested the latter yet.
    
    Asynchronous in design, only requires a 'start' signal to begin execution.

---
+++ Memory.v
@@ -0,0 +1,62 @@
+// 2016 Ryan Leonard
+// Memory Module
+//
+//
+module memory ( 
+  start,
+  write_enabled,
+  address,
+  input_data,
+  valid,
+  output_data,
+  err_invalid_address
+);
+
+// Parametrs to change the size of our system's basic
+parameter 
+  MEMORY_SIZE = 1024, 
+  WORD_SIZE = 32; // 32 --> 32bit system, 64 --> 64bit system
+
+input wire                  start;
+input wire 	                write_enabled;
+input wire [WORD_SIZE-1:0]	address;
+input wire [WORD_SIZE-1:0]	input_data;
+output reg                  valid;
+output reg [WORD_SIZE-1:0]	output_data;
+output wire                 err_invalid_address;
+
+// A data structure 
+reg [WORD_SIZE:0]  data[MEMORY_SIZE:0];
+//    |                   |
+//    v                   v
+//(datum size)      (memory size)
+
+
+initial
+begin
+  valid = 0;
+end
+
+// Begin computing on positive edge of the start signal
+always @ (posedge start) 
+begin
+  valid = 0;
+
+  // Perform write to memory if write_enabled bit is high
+  if (write_enabled) 
+  begin
+    data[address] = input_data;
+  end
+  else 
+  begin
+    // Will our critical path be just as fast if we remove the
+    // else block and do the output_data read regardless?
+    output_data = data[address];
+    valid = 1;
+  end
+end
+
+assign err_invalid_address = 
+  (address > MEMORY_SIZE-1) || (address < 0) ? 1 : 0;
+
+endmodule
---
+++ Memory_tb.v
@@ -0,0 +1,82 @@
+// 2016 Ryan Leonard
+// Memory Module Testbench
+//
+//
+module memory_tb ();
+
+// setup our memory parameters 
+localparam WORD_SIZE = 32;
+localparam MEMORY_SIZE = 1000;
+
+// The reg/nets we will maniupulate/monitor for testing
+reg clock;    
+reg write_enabled;
+wire valid;
+wire error;
+reg [WORD_SIZE-1:0]	address;  
+reg [WORD_SIZE-1:0]	input_data;
+wire [WORD_SIZE-1:0]	output_data;
+
+// build a version of the Design Under Test (dut)
+//  Instead of hand controlling the 'start' signal, we will
+//  hook it up to the clock so that every clock cycle we will
+//  attempt to perform either a read or a write to memory.
+memory
+#(
+  .WORD_SIZE (WORD_SIZE), 
+  .MEMORY_SIZE (MEMORY_SIZE)
+) dut(
+  .start          (clock),
+  .write_enabled  (write_enabled),
+  .address        (address),
+  .input_data     (input_data),
+  .valid          (valid),
+  .output_data    (output_data),
+  .err_invalid_address  (error)
+);
+
+// Clock Generator (#10 period)
+initial 
+begin
+  clock = 1; 
+  #5;
+  forever
+  begin
+    clock = ~clock; 
+    #5;
+  end
+end
+
+integer i;
+initial
+begin // BEG Test stimulus
+  #20;
+
+  $display("==========\nRead Values (offset 100) \n");
+  write_enabled = 0;
+  for (i=0; i<MEMORY_SIZE; i=i+100) 
+  begin
+    write_enabled = 0;
+    address=i; 
+    #10;
+  end
+
+  $display("==========\n Write Values (offset 200) \n");
+  for (i=0; i<MEMORY_SIZE; i=i+200) 
+  begin
+    write_enabled = 1;
+    address=i; 
+    input_data=i+1;
+    #10;
+  end
+end
+
+// Basic console output
+initial 
+begin
+  $display("Time || address || input, output || valid, error");
+  $monitor("%d || %d || %d, %d || %b, %b",
+    $time, address, input_data, output_data, valid, error);
+end
+
+endmodule
