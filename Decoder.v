6d0cdf93818438ccd31af444ec59d2785a1aa83e
Ryan <ryan.leonard71@gmail.com>
2016-05-15 19:37:38 -0700

    Removed instruction-type splitting.

--- Decoder.v
+++ Decoder.v
@@ -3,53 +3,53 @@
 
 module decoder_32(
   instruction,
-  // output for r-type instructionuctions
-  r_opcode,
-  r_rs,
-  r_rt,
-  r_rd,
-  r_shamt,
-  r_function,
-  // output for i-type instructionuctions
-  i_opcode,
-  i_rs,
-  i_rt,
-  i_immediate,
-  // output for j-type instructionuctions
-  j_opcode,
-  j_jump_target
+  opcode,
+  rs,
+  rt,
+  rd,
+  shamt,
+  alu_function,
+  immediate,
+  jump_target
 );
 
 input wire [31:0]  instruction;
 
-output wire [5:0] r_opcode;
-output wire [4:0] r_rs;
-output wire [4:0] r_rt;
-output wire [4:0] r_rd;
-output wire [4:0] r_shamt;
-output wire [5:0] r_function;
+output wire [5:0] opcode;
+output wire [4:0] rs;
+output wire [4:0] rt;
+output wire [4:0] rd;
+output wire [4:0] shamt;
+output wire [5:0] alu_function;
+output wire [15:0] immediate;
+output wire [25:0] jump_target;
 
-output wire [5:0]  i_opcode;
-output wire [4:0]  i_rs;
-output wire [4:0]  i_rt;
-output wire [15:0] i_immediate;
-
-output wire [5:0]  j_opcode;
-output wire [25:0] j_jump_target;
-
-assign r_opcode    = instruction[31:26];
-assign r_rs        = instruction[25:21];
-assign r_rt        = instruction[20:16];
-assign r_rd        = instruction[15:11];
-assign r_shamt     = instruction[10:06];
-assign r_function  = instruction[05:00];
-
-assign i_opcode     = instruction[31:26];
-assign i_rs         = instruction[25:21];
-assign i_rt         = instruction[20:16];
-assign i_immediate  = instruction[15:00];
-
-assign j_opcode       = instruction[31:26];
-assign j_jump_target  = instruction[25:00];
+//    R-type
+//  opcode   rs    rt     rd    shamt   alu_function
+//  ______ ______ ______ ______ ______ ______
+// |______|______|______|______|______|______|
+//   6bit   5bit   5bit   5bit   5bit   6bit
+//
+//
+//    I-type
+//  opcode   rs     rt     immediate
+//  ______ ______ ______ ____________________
+// |______|______|______|____________________|
+//   6bit   5bit   5bit       16bit
+//
+//    J-type
+//  opcode           jump_target
+//  ______ __________________________________
+// |______|__________________________________|
+//   6bit            26bit
+//
+assign opcode       = instruction[31:26];
+assign rs           = instruction[25:21];
+assign rt           = instruction[20:16];
+assign rd           = instruction[15:11];
+assign shamt        = instruction[10:06];
+assign alu_function = instruction[05:00];
+assign immediate    = instruction[15:00];
+assign jump_target  = instruction[25:00];
 
 endmodule
