8f02f11a1166ecee3af88eaba41d045fffee2e24
Ryan <ryan.leonard71@gmail.com>
2016-05-15 17:32:59 -0700

    Compiles!
    The basics are together and it compiles.
    
    Unsure about the design decision of having so many redundant output wires
    -- unsure how much of it can be optimized away. Also unsure of whether it
    will even be helpful to have those redundant output wires, a lot of the
    datapath should work the same regardless of which instruction type is
    input.

--- Decoder.v
+++ Decoder.v
@@ -1,16 +1,55 @@
 // 2016 Ryan Leonard
 // Decoder Module
 
-
 module decoder_32(
-  input [31:0]	instruction,
+  instruction,
+  // output for r-type instructionuctions
+  r_opcode,
+  r_rs,
+  r_rt,
+  r_rd,
+  r_shamt,
+  r_function,
+  // output for i-type instructionuctions
+  i_opcode,
+  i_rs,
+  i_rt,
+  i_immediate,
+  // output for j-type instructionuctions
+  j_opcode,
+  j_jump_target
 );
-wire op           = instr[0:0];
-wire rs           = instr[0:0];
-wire rt           = instr[0:0];
-wire rd           = instr[0:0];
-wire shamt        = instr[0:0];
-wire funct        = instr[0:0];
-wire immediate    = instr[0:0];
-wire displacement = instr[0:0];
-wire target       = instr[0:0];
+
+input wire [31:0]  instruction;
+
+output wire [5:0] r_opcode;
+output wire [4:0] r_rs;
+output wire [4:0] r_rt;
+output wire [4:0] r_rd;
+output wire [4:0] r_shamt;
+output wire [5:0] r_function;
+
+output wire [5:0]  i_opcode;
+output wire [4:0]  i_rs;
+output wire [4:0]  i_rt;
+output wire [15:0] i_immediate;
+
+output wire [5:0]  j_opcode;
+output wire [25:0] j_jump_target;
+
+assign r_opcode    = instruction[31:26];
+assign r_rs        = instruction[25:21];
+assign r_rt        = instruction[20:16];
+assign r_rd        = instruction[15:11];
+assign r_shamt     = instruction[10:06];
+assign r_function  = instruction[05:00];
+
+assign i_opcode     = instruction[31:26];
+assign i_rs         = instruction[25:21];
+assign i_rt         = instruction[20:16];
+assign i_immediate  = instruction[15:00];
+
+assign j_opcode       = instruction[31:26];
+assign j_jump_target  = instruction[25:00];
+
+endmodule
