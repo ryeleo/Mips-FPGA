4612444e5e6b0e654ed2d0525bb91e1ffcdd58b9
Ryan <ryan.leonard71@gmail.com>
2016-05-23 10:13:45 -0700

    Added files that Rui submitted via Slack

--- Pc_control.v
+++ Pc_control.v
@@ -3,18 +3,18 @@ module pc_control_32(
 	input                   reset,		            // reset bit
 	input wire              beq,	     	        // branch and zero
 	input wire	            jump,			        // if jump then 1
-	input wire [31:0]       branch_addr,	        // from branch_addr 
+	input wire [31:0]       branch_offset,	        // from branch_offset 
 	input wire [25:0]       jump_addr,		        // instruction [25:0]
 
 	output reg [31:0]       pc			            // pc out
 );
 
 	wire [1: 0]             jump_branch;
-	wire [27:0]				shifted_jump_addr;
+	wire [27:0]				extend_jump_address;
 	reg  [31:0]             pc_reg;
 	reg  [31:0]             temp_pc_reg;
 
-	assign shifted_jump_addr [25:0] = jump_addr;
+	assign extend_jump_address [25:0] = jump_addr;
 
 	always @(posedge clk) begin
 
@@ -29,14 +29,15 @@ module pc_control_32(
 			temp_pc_reg = pc_reg + 4;
 
 			if ( jump == 1 ) begin
+				$display("jump ================================================== ");
 
-				pc_reg[27 :0]  = ( jump_addr << 2 );
+				pc_reg[27 :0]  <= ( extend_jump_address << 2 );
 
-				pc_reg[31:28]  =   temp_pc_reg; 	
-			
+				pc_reg[31:28]  <=   temp_pc_reg[31:28]; 	
+
 			end else if ( beq == 1 ) begin
 				$display("beq ================================================== ");
-				pc_reg = temp_pc_reg + ( branch_addr << 2 );
+				pc_reg = temp_pc_reg + ( branch_offset << 2 );
 			
 			end else begin
 			
