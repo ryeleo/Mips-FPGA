9ca5c7434729aabbe9e758286d59af29d00e25f4
Ryan <ryan.leonard71@gmail.com>
2016-05-25 20:56:07 -0700

    Got testbench running.
    Lots of refactoring to change the 'WRITE' 'READ' state management back to
    a cleaner 2-variable system

--- Memory_tb.v
+++ Memory_tb.v
@@ -2,21 +2,21 @@
 // Memory Module Testbench
 //
 //
-module memory_tb ();
+module memory_32_test();
 
 // setup our memory parameters 
 localparam WORD_SIZE = 32;
 localparam MEMORY_SIZE = 1000;
+localparam ON   = 1'b1;
+localparam OFF  = 1'b0;
 
 // The reg/nets we will maniupulate/monitor for testing
 reg clock;    
-reg [1:0] read_write_control; // = {1 --> read_enabled, 0 --> write_enabled};
-localparam READ   = 2'b10;
-localparam WRITE  = 2'b01;
-wire valid;
-wire error;
+reg read_enabled;
+reg write_enabled;
 reg [WORD_SIZE-1:0]	address;  
 reg [WORD_SIZE-1:0]	input_data;
+wire error;
 wire [WORD_SIZE-1:0]	output_data;
 
 // build a version of the Design Under Test (dut)
@@ -28,15 +28,23 @@ memory
   .WORD_SIZE (WORD_SIZE), 
   .MEMORY_SIZE (MEMORY_SIZE)
 ) dut(
-  .start          (clock),
-  .read_enabled   (read_write_control[1]),
-  .write_enabled  (read_write_control[0]),
-  .address        (address),
+  .clock          (clock),
+  .read_enabled   (read_enabled),
+  .write_enabled  (write_enabled),
+  .input_address  (address),
   .input_data     (input_data),
-  .valid          (valid),
   .output_data    (output_data),
   .err_invalid_address  (error)
 );
+
+task reset();
+begin
+  read_enabled = OFF;
+  write_enabled = OFF;
+  address = OFF;  
+  input_data = OFF;
+end
+endtask
 
 // Clock Generator (#10 period)
 initial 
@@ -53,61 +61,66 @@ end
 integer i;
 initial
 begin // BEG Test stimulus
-  #20;
 
   $display("==========\n Write Values (offset 200) \n");
   for (i=0; i<MEMORY_SIZE; i=i+200) 
   begin
-    read_write_control = WRITE;
+    reset();
+    write_enabled = ON;
     address = i; 
     input_data = i+1;
     #10;
   end
-  input_data = 0; // for readability in the tests...
 
   $display("==========\nRead Values (offset 100) \n");
   for (i=0; i<MEMORY_SIZE; i=i+100) 
   begin
-    read_write_control = READ;
+    reset();
+    read_enabled = ON;
     address = i; 
     #10;
   end
 
   $display("==========\n Write then read max value at max addr \n");
-  read_write_control = WRITE;
+  reset();
+  write_enabled = ON;
   address = MEMORY_SIZE-1; 
   input_data = '1; // max number is all 1's
   #10;
-  read_write_control = READ;
+
+  reset();
+  read_enabled = ON;
   address = MEMORY_SIZE-1; 
-  input_data = 0; // for readability
   #10;
 
   $display("==========\n Try reading and writing to invalid address\n");
-  read_write_control = WRITE;
+  reset();
+  write_enabled = ON;
   address = MEMORY_SIZE; 
-  input_data = '0;
   #10;
-  read_write_control = READ;
+
+  reset();
+  read_enabled = ON;
   address = MEMORY_SIZE; 
-  input_data = '0; 
   #10;
-  read_write_control = WRITE;
+
+  reset();
+  write_enabled = ON;
   address = -1; 
-  input_data = '0;
   #10;
-  read_write_control = READ;
+
+  reset();
+  read_enabled = ON;
   address = -1; 
-  input_data = '0; 
   #10;
 end
 
 // Basic console output
 initial 
 begin
-  $display("Time || address || input, output || valid, error");
-  $monitor("%d || %d || %d, %d || %b, %b",
-    $time, address, input_data, output_data, valid, error);
+  $display("Time || address || input, output || error");
+  $monitor("%d || %d || %d, %d || %b",
+    $time, address, input_data, output_data, error);
 end
 
 endmodule
