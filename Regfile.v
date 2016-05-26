69eec62b4f4f079b4983286a09e275d18a5d2bdf
Ryan <ryan.leonard71@gmail.com>
2016-05-25 19:07:06 -0700

    Removed useless code & updated to run on clock.

--- Regfile.v
+++ Regfile.v
@@ -24,45 +24,34 @@ module rf_32(
   outB
 );
 
-parameter
-  OFF = 1'b0,
-  ON = 1'b1;
-
 localparam 
   REG_SIZE = 32,
   REGFILE_SIZE = 32,
   INDEX_SIZE = 5,
   ZERO = 32'b0;
 
-input wire        start;
+input wire        clock;
 input wire        read_enabled;
 input wire [INDEX_SIZE-1:0]  read_addr_s;
 input wire [INDEX_SIZE-1:0]  read_addr_t;
 input wire        write_enabled;
 input wire [INDEX_SIZE-1:0]  write_addr;
 input wire [REG_SIZE-1:0] write_data;
-output reg        finish;
 output reg [REG_SIZE-1:0] outA;
 output reg [REG_SIZE-1:0] outB;
 
 // A 'memories' data structure representing:
 //    32 registeread_addr_s each 32 bits
 reg [REG_SIZE-1:0] register_file[REGFILE_SIZE-1:0];
-//    |                   |
-//    v                   v
-//  reg_SIZE-1        reg_COUNT-1
-//  (register size)   (register file size)
 
 
-always @ (posedge start)
+always @ (posedge clock)
 begin // BEG logic
-  finish = OFF;
   outA = register_file[read_addr_s];
   outB = register_file[read_addr_t];
   if (write_enabled)
     register_file[write_addr] = write_data;
   register_file[0] = ZERO;
-  finish = ON;
 end // END logic
 
 endmodule
