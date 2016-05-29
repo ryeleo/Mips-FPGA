2e7386815be7a363e505f5e81a0d3ef246da7779
Ryan <ryan.leonard71@gmail.com>
2016-05-29 14:33:08 -0700

    Added a comment and decided on parameters

--- mipscpu.v
+++ mipscpu.v
@@ -1,27 +1,17 @@
 // 2016 Ryan Leonard & Rui Tu
 // regalumem is a composition of Register file, Alu, and data memory
+//
+// Both regfile and memory can be driven by the same clock since storeword is
+// the only instruction that writes to memory
 
-module regalumem(
+module mips_cpu(
   clock,
-  rd,
-  rs,
-  rt,
-  immediate,
-  alu_control,
-  mem_read_data,
-  mem_write_data,
+  instruction
 );
 
 input wire clock;
-input wire [4:0] rd; 
-input wire [4:0] rs; 
-input wire [4:0] rt;
-input wire [15:0] immediate;
-input wire [1:0] alu_control;
-output reg [31:0] mem_read_data;
-output reg [31:0] mem_write_data;
+input wire [31:0] instruction;
 
-// Both regfile and memory can be driven by the same clock since 
 rf_32 regfile (
   .clock(clock),
   .read_addr_s(),
