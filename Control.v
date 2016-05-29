a3f54a282d557c3209d42d68678288d1a325278a
Ryan <ryan.leonard71@gmail.com>
2016-05-28 17:20:51 -0700

    Adding Jump and link, jump register to control module
    So far have added the code and gotten both compiling, have not tested.

--- Control.v
+++ Control.v
@@ -8,14 +8,14 @@ module control_32(
 	input  wire [5:0] opcode,
 
 	output reg 	[1:0] alu_op,
-	output reg        mem_toreg,
+	output reg  [1:0] mem_toreg,
 	output reg 		  mem_write,
 	output reg 		  mem_read,
 	output reg 		  branch,
 	output reg 		  alu_src,
-	output reg 		  reg_dst,
+	output reg 	[1:0] reg_dst,
 	output reg 		  reg_write,
-	output reg 		  jump,
+	output reg 	[1:0]	jump,
 
 	output reg        err_illegal_opcode
 );
@@ -26,28 +26,49 @@ module control_32(
 			    beq 	    = 6'b0001_00,
 			    addi	    = 6'b0010_00,
 			    j    	    = 6'b0000_10,
+			    jr    	  = 6'b0010_00,
+			    jal    	  = 6'b0000_11,
 
 			    on  	    = 1'b1,
 			    off 	    = 1'b0,
+
+        /* reg_dst mux3 possible values */
+        // http://meseec.ce.rit.edu/eecc550-winter2005/550-chapter5-exercises.pdf
+			    regdst_r  	    = 2'b01,
+			    regdst_jal  	  = 2'b10,
+			    regdst_lw  	    = 2'b00,
+			    regdst_invalid  = 2'b11,
+
+        /* memtoreg mux3 possible values -- taken from same url as reg_dst */
+          memtoreg_r      = 2'b00,
+          memtoreg_jal    = 2'b10,
+          memtoreg_lw     = 2'b01,
+          memtoreg_invalid = 2'b11,
+
+        /* jump mux3 possible values -- taken from same url as reg_dst */
+          jumpmux_nojump  = 2'b00,
+          jumpmux_j_jal   = 2'b01,
+          jumpmux_jr      = 2'b10,
+          jumpmux_invalid = 2'b11,
 
 				/* 3 difference aluop */
 			    mem_alu     = 2'b00,
 			    beq_alu	    = 2'b01,
 			    artih_alu   = 2'b10,
-			    jump_code	= 2'b00;
+			    aluop_invalid	= 2'b11;
 	
 	always @(*) begin
 		case (opcode)
 			
 			r_type: begin
-				mem_toreg          <= off;
+				mem_toreg          <= memtoreg_r;
 				mem_write          <= off;
 				mem_read           <= off;
 				branch             <= off;
 				alu_src            <= off;
-				reg_dst            <= on;
+				reg_dst            <= regdst_r;
 				reg_write          <= on;
-				jump               <= off;
+				jump               <= jumpmux_nojump;
 
 				alu_op             <= artih_alu;
 				err_illegal_opcode <= off;
@@ -55,14 +76,14 @@ module control_32(
 			end
 
 			lw: begin
-				mem_toreg          <= on;
+				mem_toreg          <= memtoreg_lw;
 				mem_write          <= off;
 				mem_read           <= on;
 				branch             <= off;
 				alu_src            <= on;
-				reg_dst            <= off;
+				reg_dst            <= regdst_lw;
 				reg_write          <= on;
-				jump               <= off;
+				jump               <= jumpmux_nojump;
 				
 				alu_op             <= mem_alu;
 				err_illegal_opcode <= off;
@@ -70,14 +91,14 @@ module control_32(
 			end
 
 			sw: begin
-				mem_toreg          <= off;
+				mem_toreg          <= memtoreg_invalid;
 				mem_write          <= on;
 				mem_read           <= off;
 				branch             <= off;
 				alu_src            <= on;
-				reg_dst            <= off;
+				reg_dst            <= regdst_invalid;
 				reg_write          <= off;
-				jump               <= off;
+				jump               <= jumpmux_nojump;
 
 				alu_op     		   <= mem_alu;
 				err_illegal_opcode <= off;
@@ -85,14 +106,14 @@ module control_32(
 			end
 
 			beq: begin
-				mem_toreg          <= off;
+				mem_toreg          <= memtoreg_invalid;
 				mem_write          <= off;
 				mem_read           <= off;
 				branch             <= on;
 				alu_src            <= off;
-				reg_dst            <= off;
+				reg_dst            <= regdst_invalid;
 				reg_write          <= off;
-				jump               <= off;
+				jump               <= jumpmux_nojump;
 
 				alu_op             <= beq_alu;
 				err_illegal_opcode <= off;
@@ -100,47 +121,73 @@ module control_32(
 			end
 
 			addi: begin
-				mem_toreg          <= off;
+				mem_toreg          <= memtoreg_invalid;
 				mem_write          <= off;
 				mem_read           <= off;
 				branch             <= off;
 				alu_src            <= on;
-				reg_dst            <= off;
+				reg_dst            <= regdst_invalid;
 				reg_write          <= on;
-				jump               <= off;
+				jump               <= jumpmux_nojump;
 
 				alu_op             <= mem_alu;
 				err_illegal_opcode <= off;
+			end
 
+			jr: begin
+				mem_toreg          <= memtoreg_jal;
+				mem_write          <= off;
+				mem_read           <= off;
+				branch             <= off;
+				alu_src            <= off;
+				reg_dst            <= regdst_jal;
+				reg_write          <= on;
+				jump 	             <= jumpmux_jr;
+
+				alu_op             <= aluop_invalid;
+				err_illegal_opcode <= off;
+			end
+
+			jal: begin
+				mem_toreg          <= memtoreg_jal;
+				mem_write          <= off;
+				mem_read           <= off;
+				branch             <= off;
+				alu_src            <= off;
+				reg_dst            <= regdst_jal;
+				reg_write          <= on;
+				jump 	             <= jumpmux_j_jal;
+
+				alu_op             <= aluop_invalid;
+				err_illegal_opcode <= off;
 			end
 
 			j: begin
-				mem_toreg          <= off;
+				mem_toreg          <= memtoreg_invalid;
 				mem_write          <= off;
 				mem_read           <= off;
 				branch             <= off;
 				alu_src            <= off;
-				reg_dst            <= off;
+				reg_dst            <= regdst_invalid;
 				reg_write          <= off;
-				jump               <= on;
+				jump               <= jumpmux_j_jal;
 
 				jump 	           <= on;
-				alu_op             <= jump_code;
+				alu_op             <= aluop_invalid;
 				err_illegal_opcode <= off;
-
 			end
 
 			default: begin 
-				mem_toreg          <= off;
+				mem_toreg          <= memtoreg_invalid;
 				mem_write          <= off;
 				mem_read           <= off;
 				branch             <= off;
 				alu_src            <= off;
-				reg_dst            <= off;
+				reg_dst            <= regdst_invalid;
 				reg_write          <= off;
-				jump               <= off;
+				jump               <= jumpmux_invalid;
 
-				alu_op             <= 2'b00;
+				alu_op             <= aluop_invalid;
 				err_illegal_opcode <= on;
 				$display("cannot decode instruction %b\n", opcode);
 			end
--- Control_tb.v
+++ Control_tb.v
@@ -2,17 +2,17 @@ module control_test;
   reg   [5:0] opcode;
 
   wire 	[1:0] alu_op;
-  wire        mem_toreg;
+  wire  [1:0] mem_toreg;
   wire 				mem_write;
   wire 				mem_read;
   wire 				branch;
   wire 				alu_src;
-  wire 				reg_dst;
+  wire 	[1:0] reg_dst;
   wire 				reg_write;
-  wire 				jump;
+  wire 	[1:0] jump;
   wire        error;
 
-  control_32 control (
+  control_32 dut (
     .opcode(opcode),
     .alu_op(alu_op),
     .mem_toreg(mem_toreg),
@@ -26,15 +26,7 @@ module control_test;
     .err_illegal_opcode(error)
   );
  
-  wire [7:0] result;
-  assign result[7] = reg_write;
-  assign result[6] = reg_dst;
-  assign result[5] = alu_src;
-  assign result[4] = branch;
-  assign result[3] = mem_write;
-  assign result[2] = mem_read;
-  assign result[1] = mem_toreg;
-  assign result[0] = jump;
+  wire [10:0] result = {reg_write, reg_dst, alu_src, branch, mem_write, mem_read, mem_toreg, jump};
 
   initial begin
     $dumpfile("control_test.vcd");
@@ -43,11 +35,12 @@ module control_test;
     $monitor("Opcode: %b, result: %b, ALUop: %b, error: %b", opcode, result, alu_op, error); 
     opcode <= 6'b0000_00; #5; // r_type 
 
-    opcode <= 6'b1000_11; #5;// lw
-    opcode <= 6'b1010_11; #5;// sw
-    opcode <= 6'b0001_00; #5;// beq 
-    opcode <= 6'b0010_00; #5;// addi  
-    opcode <= 6'b0000_10; #5;// jump
+    opcode <= dut.lw; #5;// lw
+    opcode <= dut.sw; #5;// sw
+    opcode <= dut.beq; #5;// beq 
+    opcode <= dut.addi; #5;// addi  
+    opcode <= dut.j; #5;// jump
+    opcode <= dut.jal; #5;// jal
 
     $display("\nError codes\n\n");
     opcode <= 6'b0011_10; #5;// r_type 
