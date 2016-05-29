d8f8eb24d9b8df1bb32ea31e8a40e5f975d0a2a3
Ryan <ryan.leonard71@gmail.com>
2016-05-28 18:14:21 -0700

    Made testbench pretty and added csv table.
    Tested like a boss -- checked by Rui and Ryan

--- Control_tb.v
+++ Control_tb.v
@@ -32,16 +32,26 @@ module control_test;
     $dumpfile("control_test.vcd");
     $dumpvars(0, control_test);
 
-    $monitor("Opcode: %b, result: %b, ALUop: %b, error: %b", opcode, result, alu_op, error); 
+    $display("Opcode, result, ALUop, mem_toreg, mem_read, branch, alu_src, reg_dst, reg_write, jump, error"); 
+    $monitor("%b,%b,%b,%d,%b,%b,%b,%d,%b,%d,%b", opcode, result, alu_op, mem_toreg, mem_read, branch, alu_src, reg_dst, reg_write, jump, error); 
     opcode <= 6'b0000_00; #5; // r_type 
 
+    $display("LW");
     opcode <= dut.lw; #5;// lw
+    $display("SW");
     opcode <= dut.sw; #5;// sw
+    $display("BEQ");
     opcode <= dut.beq; #5;// beq 
+    $display("BNE");
     opcode <= dut.bne; #5;// bne
+    $display("ADDI");
     opcode <= dut.addi; #5;// addi  
+    $display("J");
     opcode <= dut.j; #5;// jump
+    $display("JAL");
     opcode <= dut.jal; #5;// jal
+    $display("JR");
+    opcode <= dut.jr; #5;// jr
 
     $display("\nError codes\n\n");
     opcode <= 6'b0011_10; #5;// r_type 
