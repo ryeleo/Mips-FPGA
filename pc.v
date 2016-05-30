c2abbbd7774710ccaf13b938ce96eacf47c0ec2e
Rui Tu <ruit@uoregon.edu>
2016-05-30 16:22:34 -0700

    we added a comment in pc.v indicates that pc won't updated until the
    next rising edge.

--- pc.v
+++ pc.v
@@ -6,17 +6,27 @@ module pc(
 	pc_out
 );
 
+  
 	reg [31:0] pc_reg;
+
 	input wire clk;
 	input wire reset;
 	input wire [31:0] pc_in;
 	output reg [31:0] pc_out;
 
+
+  // we assume the output of the jumpmux will be
+  // stable until the start of the posedge of the next
+  // clock cycle
 	always@(posedge clk) begin
 		if (reset)
+    begin
 			pc_reg = 32'h0;
+    end
 		else
+    begin
 			pc_reg = pc_in;
+    end
 		
 		pc_out = pc_reg;
 	end
