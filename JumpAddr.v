04db81fcaf31abbfef1eb4b0824a84695b466800
Rui Tu <ruit@uoregon.edu>
2016-05-30 16:14:28 -0700

    fixed jumaddr indention problem

--- JumpAddr.v
+++ JumpAddr.v
@@ -9,12 +9,12 @@ module jump_addr (
 );
 
 input wire [25:0] jump_relative_addr;
-input wire [3:0] pc_upper;
+input wire [3:0]  pc_upper;
 output reg [31:0] jump_addr;
 
 always @(*)
 begin
-  jump_addr[27:0] <= jump_relative_addr << 2;
+  jump_addr[27:0]  <= jump_relative_addr << 2;
   jump_addr[31:28] <= pc_upper;
 end
 
