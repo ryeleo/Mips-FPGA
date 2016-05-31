31b88b7d6a246a2ed8b985dd8f34692648597ed8
Rui Tu <ruit@uoregon.edu>
2016-05-30 19:28:39 -0700

    All mem access first divide by 4 (word addressing)
    We expect the incoming binaries to use byte addressing, and we update our
    PC based on byte addressing logic, but our memory modules are word
    (4byte) addressed.
    
    Our elegant fix is to right shift by 2 (divide by 4) all memory address
    accesses.
    
    ALSO! Tests from original datapath (lab2 release) pass!
    
    All instructions except branch and jump are working!
    * Add
    * Addi
    * LW
    * SW
    
    Checked by Ryan and Rui

--- tb/cpu_tb.v
+++ tb/cpu_tb.v
@@ -42,9 +42,9 @@ task assert_equal(
   input [31:0] observed);
   begin
     if (expected === observed)
-      $display("SUCCESS:            %p == %p", expected, observed);
+      $display("(%d)SUCCESS:            %p == %p", $time, expected, observed);
     else
-      $display("ASSERTION EQUAL FAIL: %p != %p", expected, observed);
+      $display("(%d)ASSERTION EQUAL FAIL: %p != %p", $time, expected, observed);
   end
 endtask
 
@@ -52,13 +52,22 @@ task load_instr (
   input [31:0] addr, 
   input [31:0] instruction
 );
-  dut.instruction_memory.data[addr] = instruction;
+  dut.instruction_memory.data[addr >> 2] = instruction;
 endtask
 
 // Test logic
 initial begin
   $display("Initializing instruction memory");
   load_instr(0, addi_t0_zero_6);
+  load_instr(4, addi_t1_zero_11);
+  load_instr(8, addi_t0_t0_10);
+  load_instr(12, addi_t2_t1_240);
+  load_instr(16, add_t2_t0_t1);
+  load_instr(20, addi_t0_zero_5);
+  load_instr(24, addi_t1_zero_9);
+  load_instr(28, sw_t0_zero_0);
+  load_instr(32, sw_t1_zero_4);
+  load_instr(36, lw_t0_zero_4);
   // Don't need these since we are adding to the zero register
   //dut.regfile.register_file[8] = 2;
   //dut.regfile.register_file[9] = 3;
@@ -69,51 +78,32 @@ initial begin
 
   $display("Running instructions!");
   reset = 0;
-  #10;
+
   assert_equal(dut.regfile.register_file[t0], 6);
+  #10;
   $display("PC: ");
   assert_equal(dut.pc.pc_reg, 4);
-  
 
-  /*
-  instruction = addi_t1_zero_11; 
-  #10;
   assert_equal(dut.regfile.register_file[t1], 11);
-
-  instruction = addi_t0_t0_10; 
   #10;
   assert_equal(dut.regfile.register_file[t0], 16);
-
-  instruction = addi_t2_t1_240; 
   #10;
   assert_equal(dut.regfile.register_file[t2], 251);
-
-  instruction = add_t2_t0_t1; 
   #10;
   assert_equal(dut.regfile.register_file[t2], 27);
+  #10;
 
   $display("We started the second iteration");
-
-  instruction = addi_t0_zero_5; 
-  #10;
   assert_equal(dut.regfile.register_file[t0], 5);
-
-  instruction = addi_t1_zero_9; 
   #10;
   assert_equal(dut.regfile.register_file[t1], 9);
-
-  instruction = sw_t0_zero_0;  // storing t0 to memory[0]
   #10;
   assert_equal(dut.data_memory.data[0], 5);
-
-  instruction = sw_t1_zero_4; // storing t1 to memory[4]
   #10;
-  assert_equal(dut.data_memory.data[4], 9);
-  
-  instruction = lw_t0_zero_4;  // loading memory[4] into t0
+  assert_equal(dut.data_memory.data[1], 9); // Store into the '4th' byte address which is our 1st word address
   #10;
   assert_equal(dut.regfile.register_file[t0], 9);
-  */
+  #10;
 end
           /*
 
