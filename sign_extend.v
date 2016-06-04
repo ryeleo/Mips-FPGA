40a902de03647580b5220d2887ca655cf0240970
CAES-LAB-4 <CAES-LAB-4>
2016-06-04 11:19:37 -0700

    fixed '0 (systemverilog) to 1 verilog in sign_extend.v
    Check with Ryan and Rui

--- sign_extend.v
+++ sign_extend.v
@@ -6,8 +6,8 @@ module sign_extend_32(
 always @(*) begin
   result_32[15:0] <= input_16;
   case (input_16[15]) 
-    1'b0: result_32 [31:16] <= '0;
-    1'b1: result_32 [31:16] <= '1;
+    1'b0: result_32 [31:16] <= 16'h0000;
+    1'b1: result_32 [31:16] <= 16'h1111;
   endcase
 end
 endmodule
